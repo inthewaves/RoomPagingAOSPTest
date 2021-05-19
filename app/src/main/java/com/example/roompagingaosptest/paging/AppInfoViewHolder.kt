fe3e0461d35250ffb6be903cb83c61e3f87fee22
inthewaves <inthewaves@pm.me>
2021-05-18 21:15:18 -0700

    get rid of dead code

--- app/src/main/java/com/example/roompagingaosptest/paging/AppInfoViewHolder.kt
+++ app/src/main/java/com/example/roompagingaosptest/paging/AppInfoViewHolder.kt
@@ -8,13 +8,10 @@ import android.widget.TextView
 import androidx.annotation.UiThread
 import androidx.core.view.isVisible
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.await
 import com.example.roompagingaosptest.MainActivityViewModel
@@ -22,7 +19,6 @@ import com.example.roompagingaosptest.R
 import com.example.roompagingaosptest.db.AppInfo
 import com.example.roompagingaosptest.db.ProgressDatabase
 import com.example.roompagingaosptest.work.AppVersionUpdateJob
-import com.example.roompagingaosptest.work.WorkerProgress
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
@@ -39,8 +35,6 @@ class AppInfoViewHolder(
 ) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.app_info_item, parent, false)
 ) {
-    private val applicationContext = parent.context.applicationContext
-    private val progressWatcher = AppVersionUpdateJob.UpdaterWatcher.getInstance(applicationContext)
     companion object {
         private const val TAG = "AppInfoViewHolder"
     }
@@ -62,11 +56,7 @@ class AppInfoViewHolder(
         progressBar.max = 100
     }
     private val workManager = WorkManager.getInstance(itemView.context)
-    private var progressLiveData: LiveData<List<WorkInfo>>? = null
-
-    private var newProgressLiveData: LiveData<WorkerProgress>? = null
-
-    private val database = ProgressDatabase.getInstance(applicationContext)
+    private val database = ProgressDatabase.getInstance(parent.context)
 
     private fun relaunchUpdateObserverJob(packageName: String) {
         progressJob?.cancel()
@@ -86,64 +76,8 @@ class AppInfoViewHolder(
                 }
         }
     }
-
     private var progressJob: Job? = null
 
-    private val newObserver = Observer<WorkerProgress> { progress ->
-        return@Observer
-
-        if (progress == WorkerProgress.FINISHED) {
-            progressBar.isVisible = false
-            return@Observer
-        }
-        progressBar.isVisible = true
-        progressBar.progress = (100 * progress.percentage).roundToInt()
-    }
-
-    private val updateObserver = object : Observer<List<WorkInfo>> {
-        private var job: Job? = null
-        private var isNewAppInfo = true
-        fun toggleNewAppData() {
-            isNewAppInfo = true
-        }
-
-        override fun onChanged(workInfoList: List<WorkInfo>?) {
-            return
-
-            val workInfo = workInfoList?.find { !it.state.isFinished }
-            if (workInfo == null) {
-                Log.d(TAG, "${appInfo?.packageName}: FAILED TO FIND IN PROGRESS JOB")
-                progressBar.isVisible = false
-                return
-            }
-
-            val progress = AppVersionUpdateJob.Progress(workInfo.progress)
-            Log.d(
-                TAG,
-                "${appInfo?.packageName} Observing: ${progress.percentage} (isNewAppInfo: $isNewAppInfo)\n" +
-                        "    The list if $workInfoList"
-            )
-
-            val animated = !isNewAppInfo
-            if (workInfo.state.isFinished) {
-                if (progressBar.isVisible) {
-                    progressBar.isVisible = false
-                    progressBar.setProgressCompat(0, animated)
-                    stopObserving()
-                } else {
-                    stopObserving()
-                }
-            } else {
-                progressBar.isVisible = true
-                progressBar.setProgressCompat(
-                    (100 * progress.percentage).roundToInt(),
-                    animated
-                )
-            }
-            isNewAppInfo = false
-
-        }
-    }
 
     init {
         updateButton.setOnClickListener {
@@ -160,17 +94,6 @@ class AppInfoViewHolder(
                     workRequest
                 )
 
-                /*
-                progressLiveData?.removeObserver(updateObserver)
-                progressLiveData = workManager.getWorkInfosForUniqueWorkLiveData(AppVersionUpdateJob.createName(it))
-                    .apply { observe(lifecycle, updateObserver) }
-
-                 */
-
-                newProgressLiveData?.removeObserver(newObserver)
-                newProgressLiveData = progressWatcher.getOrCreateProgressForPackage(it.packageName)
-                    .apply { observe(lifecycle, newObserver) }
-                Log.d(TAG, "Progress is ${newProgressLiveData?.value}")
 
                 lifecycle.lifecycleScope.launch {
                     val list = workManager
@@ -199,52 +122,15 @@ class AppInfoViewHolder(
     @UiThread
     fun stopObserving() {
         Log.d(TAG, "stopObserving(): appInfo=$appInfo")
-        appInfo?.let {
-            progressWatcher.removePackageIfComplete(it.packageName)
-        }
-
-
-        // progressBar.isVisible = false
-        /*
-        progressBar.setProgressCompat(0, false)
-        updateObserver.toggleNewAppData()
-        progressLiveData?.removeObserver(updateObserver)
-        progressLiveData = null
-         */
-
-        newProgressLiveData?.removeObservers(lifecycle)
-
         progressJob?.cancel()
     }
 
     @UiThread
     fun bind(appInfo: AppInfo?) {
-        updateObserver.toggleNewAppData()
-
-        /*
-        progressLiveData?.removeObserver(updateObserver)
-        if (appInfo == null) {
-            Log.d(TAG, "FAILED TO FIND APP INFO")
-        }
-        progressLiveData = appInfo?.let {
-            workManager.getWorkInfosForUniqueWorkLiveData(AppVersionUpdateJob.createName(it))
-                .apply {
-                    observe(lifecycle, updateObserver)
-                }
-        }
-         */
         this.appInfo = appInfo
-        newProgressLiveData?.removeObserver(newObserver)
-        newProgressLiveData = appInfo
-            ?.let { progressWatcher.getOrCreateProgressForPackage(it.packageName) }
-            ?.apply {
-                observe(lifecycle, newObserver)
-                newObserver.onChanged(this.value)
-            }
         appName.text = appInfo?.packageName ?: ""
         versionCode.text = appInfo?.versionCode?.toString() ?: ""
         lastUpdate.text = appInfo?.lastUpdated?.toString() ?: ""
-
         appInfo?.let { relaunchUpdateObserverJob(it.packageName) }
     }
 }
--- app/src/main/java/com/example/roompagingaosptest/work/AppVersionUpdateJob.kt
+++ app/src/main/java/com/example/roompagingaosptest/work/AppVersionUpdateJob.kt
@@ -3,25 +3,17 @@ package com.example.roompagingaosptest.work
 import android.content.Context
 import android.content.pm.PackageManager
 import android.util.Log
-import androidx.annotation.VisibleForTesting
-import androidx.collection.arrayMapOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.room.withTransaction
 import androidx.work.CoroutineWorker
 import androidx.work.Data
-import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import com.example.roompagingaosptest.db.AppInfo
 import com.example.roompagingaosptest.db.ProgressDatabase
 import com.example.roompagingaosptest.db.TestDatabase
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.withContext
 import java.io.File
 
@@ -59,96 +51,36 @@ class AppVersionUpdateJob(
         val appUpdateProgressDao = ProgressDatabase.getInstance(applicationContext)
             .appUpdateProgressDao()
         val input = Input(inputData)
-        val updaterWatcher = UpdaterWatcher.getInstance(applicationContext)
-        val progress = updaterWatcher.getOrCreateProgressForPackage(input.pkg) as MutableLiveData<WorkerProgress>
         var percentage: Double = 0.0
         // setProgress(Progress(0.0).progressData)
         appUpdateProgressDao.updateProgressForPackage(input.pkg, 0.0)
-        progress.postValue(WorkerProgress.ZERO)
         repeat(10 * 33) {
             delay(50L)
             percentage += 0.002
-            // setProgress(Progress(percentage).progressData)
-            progress.percentage = percentage
             appUpdateProgressDao.updateProgressForPackage(input.pkg, percentage)
         }
-        // setProgress(Progress(2/3.0).progressData)
-        progress.percentage = 2/3.0
         appUpdateProgressDao.updateProgressForPackage(input.pkg, 2/3.0)
-        delay(2500L)
 
-        val success =  coroutineScope {
-            val progressActor = actor<Double>(capacity = Channel.CONFLATED) {
-                for (percentage in channel) {
-                    appUpdateProgressDao.updateProgressForPackage(input.pkg, percentage)
-                }
-            }
+        val success = coroutineScope {
 
             val success = database.withTransaction {
                 val dao = database.appInfoDao()
                 val appInfo = dao.getAppInfo(input.pkg) ?: return@withTransaction false
                 dao.updateVersionCode(appInfo.packageName, appInfo.versionCode + 1L)
-                // setProgress(Progress(0.9).progressData)
-                progress.percentage = 0.9
-                progressActor.offer(0.9)
+                appUpdateProgressDao.updateProgressForPackage(input.pkg, 0.9)
                 true
             }
-            progressActor.close()
-
             success
         }
         delay(2500L)
-        // setProgress(Progress(1.0).progressData)
-        progress.postValue(WorkerProgress.FINISHED)
         appUpdateProgressDao.updateProgressForPackage(input.pkg, 1.0)
         withContext(Dispatchers.Main) {
             Log.d(WORK_TAG, "${input.pkg} has reached 100%")
         }
         delay(2500L)
 
-        updaterWatcher.removePackage(input.pkg)
         appUpdateProgressDao.deletePackage(input.pkg)
         return if (success) Result.success() else Result.failure()
-    }
-
-    class UpdaterWatcher private constructor(context: Context) {
-        private val mapMutex = Mutex()
-        private val appInfoMap: MutableMap<String, LiveData<WorkerProgress>> = arrayMapOf()
-        private val workManager = WorkManager.getInstance(context)
-
-        @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
-        fun removePackage(pkg: String) {
-            synchronized(appInfoMap) { appInfoMap.remove(pkg) }
-        }
-
-        fun removePackageIfComplete(pkg: String) {
-            synchronized(appInfoMap) {
-                if (appInfoMap[pkg]?.value == WorkerProgress.FINISHED) {
-                    appInfoMap.remove(pkg)
-                }
-            }
-        }
-
-        fun getProgressForPackageOrNull(pkg: String): LiveData<WorkerProgress>? =
-            synchronized(appInfoMap) {
-                appInfoMap[pkg]
-            }
-
-        fun getOrCreateProgressForPackage(pkg: String): LiveData<WorkerProgress> =
-            synchronized(appInfoMap) {
-                appInfoMap.getOrPut(
-                    pkg,
-                    { MutableLiveData(WorkerProgress.FINISHED) }
-                )
-            }
-
-        companion object {
-            private var instance: UpdaterWatcher? = null
-            fun getInstance(context: Context) =
-                instance ?: synchronized(UpdaterWatcher::class.java) {
-                    UpdaterWatcher(context).also { instance = it }
-                }
-        }
     }
 
     class Input constructor(val inputData: Data) {
@@ -160,32 +92,5 @@ class AppVersionUpdateJob(
             private const val PACKAGE_KEY = "package"
         }
     }
-
-    class Progress constructor(val progressData: Data) {
-        constructor(percentage: Double): this(workDataOf(PROGRESS_PERCENT_KEY to percentage))
-
-        val percentage: Double
-            get() = progressData.getDouble(PROGRESS_PERCENT_KEY, 0.0)
-
-        companion object {
-            const val PROGRESS_PERCENT_KEY = "percent"
-        }
-    }
 }
-
-inline class WorkerProgress(val percentage: Double) {
-    val isFinished: Boolean
-        get() = this == FINISHED || percentage == -1.0
-
-    operator fun plus(other: WorkerProgress) = WorkerProgress(percentage + other.percentage)
-
-    companion object {
-        val ZERO = WorkerProgress(0.0)
-        val FINISHED = WorkerProgress(-1.0)
-    }
-}
-
-var MutableLiveData<WorkerProgress>.percentage: Double
-    get() = this.value?.percentage ?: 0.0
-    set(value) = postValue(WorkerProgress(value))
 
